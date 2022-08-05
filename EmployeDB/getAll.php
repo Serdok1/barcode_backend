26c6f696ca4a428c34a8ca8fd3e5135d3eea1a32
Serdok1 <69848278+Serdok1@users.noreply.github.com>
2022-08-05 16:15:14 +0300

    fri3

--- EmployeDB/getAll.php
+++ EmployeDB/getAll.php
@@ -3,11 +3,16 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "barcode";
-$table = "employees";
+$table = "deneme";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
 $db_data = array();
-$query = $conn->query("SELECT * FROM `$table` ");
+$query = $conn->query("SELECT * FROM `$table`");
 while ($rowData = $query->fetch_assoc()) {
     $db_data[] = $rowData;
 }
